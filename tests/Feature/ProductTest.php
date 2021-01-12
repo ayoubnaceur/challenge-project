431e9d5a3a48cfc9917a73ad83010ab85a346afe
Ayoub Naceur <ayoubnaceur@users.noreply.github.com>
2021-01-12 12:21:49 +0100

    Product creation testing
    - Create unit tests for product creation

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductTest extends TestCase
+{
+    /**
+     * Test if the app route is here.
+     *
+     * @return void
+     */
+    public function test_application_ready()
+    {
+        $response = $this->get('route("any")');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test if we can create a new product.
+     *
+     * @return void
+     */
+    public function test_create_new_product_success()
+    {
+        Storage::fake('avatars');
+
+        $file = UploadedFile::fake()->image('avatar.jpg');
+
+        $response = $this->postJson(route('products.store'), [
+            // u use the hash of the created file to do not get unique name error in validation
+            'name' => substr($file->hashName(), 0, 10),
+            'description' => 'productX description',
+            'price' => 100,
+            'categories' => '[1,2]',
+            'image' => $file,
+        ]);
+
+        // the first run of this test sould give
+
+        $response->assertStatus(201);
+        Storage::disk('public')->assertExists('products/'.$file->hashName());
+
+    }
+
+    /**
+     * Should not be created (I check that validation works fine & not any product stored).
+     *
+     * @return void
+     */
+    public function test_create_new_product_failed()
+    {
+        $response = $this->postJson(route('products.store'), [
+            'name' => 'xxxxxxxxxxxxxx',
+        ]);
+
+        // In the case the product is arleady exists we will get 422 status code
+        $response->assertStatus(422);
+
+        // see the problem (incopleted form)
+        $response->dump();
+    }
+}
